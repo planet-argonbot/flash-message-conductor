afca5d5e7a45b1dfc4138c0a1e3f2f71834be105
Robby Russell <robby@planetargon.com>
2008-08-27 19:57:57 -0700

    Initial work on adding view and controller helpers for flash messages

--- init.rb
+++ init.rb
@@ -1,1 +1,10 @@
 # Include hook code here
+require File.dirname(__FILE__) + '/lib/argon_helpers'
+
+ActionController::Base.send( :include, PlanetArgon::ControllerHelpers )
+ActionView::Base.send( :include, PlanetArgon::ViewHelpers )
+
+# override field errors
+ActionView::Base.field_error_proc = 
+         Proc.new{ |html_tag, instance| "<span class=\"field_with_errors\">#{html_tag}</span>" }
+
--- lib/argon_helpers.rb
+++ lib/argon_helpers.rb
@@ -1,1 +1,36 @@
 # ArgonHelpers
+
+module PlanetArgon 
+  module ControllerHelpers
+    def add_error(msg)
+      flash[:error] = msg
+    end
+
+    def add_notice(msg)
+      flash[:notice] = msg
+    end
+
+    def add_message(msg)
+      flash[:message] = msg
+    end
+  end
+  
+  module ViewHelpers
+    
+    FLASH_MESSAGE_TYPES = [ :error, :notice, :message ]
+    
+    def render_flash_message( css_class, message = "" ) 
+      return "" if message.nil? or message.blank?
+      content_tag( "p", message, :class => "#{css_class}" )
+    end
+    
+    def render_flash_messages( div_id = "flash_messages"  )
+      div_content = ''
+      FLASH_MESSAGE_TYPES.each do |key|
+        div_content << render_flash_message( key.to_s, flash[key] ) unless flash[key].blank?
+      end
+      content_tag( 'div', div_content, :id => div_id )
+    end
+    
+  end
+end
