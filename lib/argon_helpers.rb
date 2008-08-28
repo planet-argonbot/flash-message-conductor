dbbd5e63b4a79ab0261ed92994c66957705030cd
Robby Russell <robby@planetargon.com>
2008-08-27 23:08:12 -0700

    Adding another namespace for this plugin

--- lib/argon_helpers.rb
+++ lib/argon_helpers.rb
@@ -1,36 +1,36 @@
 # ArgonHelpers
 
 module PlanetArgon 
-  module ControllerHelpers
-    def add_error(msg)
-      flash[:error] = msg
-    end
-
-    def add_notice(msg)
-      flash[:notice] = msg
-    end
-
-    def add_message(msg)
-      flash[:message] = msg
-    end
-  end
-  
-  module ViewHelpers
-    
+  module FlashMessageConductor
     FLASH_MESSAGE_TYPES = [ :error, :notice, :message ]
     
-    def render_flash_message( css_class, message = "" ) 
-      return "" if message.nil? or message.blank?
-      content_tag( "p", message, :class => "#{css_class}" )
-    end
-    
-    def render_flash_messages( div_id = "flash_messages"  )
-      div_content = ''
-      FLASH_MESSAGE_TYPES.each do |key|
-        div_content << render_flash_message( key.to_s, flash[key] ) unless flash[key].blank?
+    module ControllerHelpers
+      def add_error(msg)
+        flash[:error] = msg
       end
-      content_tag( 'div', div_content, :id => div_id )
+
+      def add_notice(msg)
+        flash[:notice] = msg
+      end
+
+      def add_message(msg)
+        flash[:message] = msg
+      end
     end
+  
+    module ViewHelpers
+      def render_flash_message( css_class, message = "" ) 
+        return "" if message.nil? or message.blank?
+        content_tag( "p", message, :class => "#{css_class}" )
+      end
     
+      def render_flash_messages( div_id = "flash_messages"  )
+        div_content = ''
+        FLASH_MESSAGE_TYPES.each do |key|
+          div_content << render_flash_message( key.to_s, flash[key] ) unless flash[key].blank?
+        end
+        content_tag( 'div', div_content, :id => div_id )
+      end
+    end
   end
 end
