fa8655afb7bbb11a4356f700b3625c60a467654f
robbyrussell <robby@planetargon.com>
2008-08-29 22:30:46 -0700

    Handling when there are no flash messages

--- lib/flash_message_conductor.rb
+++ lib/flash_message_conductor.rb
@@ -29,7 +29,11 @@ module PlanetArgon
         FLASH_MESSAGE_TYPES.each do |key|
           div_content << render_flash_message( key.to_s, flash[key] ) unless flash[key].blank?
         end
-        content_tag( 'div', div_content, :id => div_id )
+        if div_content.blank?
+          return ""
+        else
+          return content_tag( 'div', div_content, :id => div_id )
+        end
       end
     end
   end
