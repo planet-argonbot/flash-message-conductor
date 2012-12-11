049c1c1a50709c0168b908a88b82103c0a6bd613
Carlos Rodriguez <carlos@eddorre.com>
2012-12-10 18:38:28 -0800

    Both message and div content are considered HTML safe

--- lib/flash-message-conductor/view_helpers.rb
+++ lib/flash-message-conductor/view_helpers.rb
@@ -2,18 +2,18 @@ module FlashMessageConductor
   module ViewHelpers
     def render_flash_message(css_class, message = "")
       return "" if message.nil? or message.blank?
-      content_tag("p", message, :class => "#{css_class}")
+      content_tag("p", message.html_safe, :class => "#{css_class}")
     end
 
     def render_flash_messages(div_id = "flash_messages", div_class = "")
-      div_content = ''.html_safe
+      div_content = ''
       FLASH_MESSAGE_TYPES.each do |key|
         div_content << render_flash_message(key.to_s, flash[key]) unless flash[key].blank?
       end
       if div_content.blank?
         return ""
       else
-        return content_tag('div', div_content, :id => div_id, :class => div_class)
+        return content_tag('div', div_content.html_safe, :id => div_id, :class => div_class)
       end
     end
 
