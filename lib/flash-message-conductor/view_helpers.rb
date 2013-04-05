aa1c92d77bd4e20d4f413ed96b3961eb33c09c0b
Carlos Rodriguez <carlos@eddorre.com>
2013-04-05 11:30:03 -0700

    Using flash_message_set? to shortcut helper method render_flash_messages if there are no flash messages.

--- lib/flash-message-conductor/view_helpers.rb
+++ lib/flash-message-conductor/view_helpers.rb
@@ -6,12 +6,13 @@ module FlashMessageConductor
     end
 
     def render_flash_messages(div_id = "flash_messages", div_class = "")
+      return unless flash_message_set?
+
       div_content = ''
       FLASH_MESSAGE_TYPES.each do |key|
         div_content << render_flash_message(key.to_s, flash[key]) unless flash[key].blank?
       end
 
-      return "" if div_content.blank?
       content_tag('div', div_content.html_safe, :id => div_id, :class => div_class)
     end
   end
